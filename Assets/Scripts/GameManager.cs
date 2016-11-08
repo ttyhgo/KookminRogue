09eb4cd912cf46578b0b89e51ad9ea2500f26025
max <ttyhgo@gmail.com>
2016-11-08 16:08:24 +0900

    Rogulike study 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	private BoardManager boardManager;
+	private int level = 3;
+
+	// Use this for initialization
+	void Awake () {
+
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+		
+		DontDestroyOnLoad (gameObject);
+		boardManager = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void InitGame(){
+		boardManager.SetupScene (level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
