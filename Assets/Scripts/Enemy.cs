a97d898d90c39a640c5e4a6c2a64a25a7f5722d7
max <ttyhgo@gmail.com>
2016-11-24 12:55:40 +0900

    Fin study

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,6 +9,9 @@ public class Enemy : MovingObject {
 	private Transform target;
 	private bool skipMove;
 
+	public AudioClip enemyAttack1;
+	public AudioClip enemyAttack2;
+
 	protected override void Start () {
 		GameManager.instance.AddEnemyToList (this);
 		animator = GetComponent<Animator> ();
@@ -42,5 +45,7 @@ public class Enemy : MovingObject {
 		Player hitPlayer = component as Player;
 		hitPlayer.LoseFood (playerDamage);
 		animator.SetTrigger ("EnemyAttack");
+
+		SoundManager.instance.RandomizeSfx (enemyAttack1, enemyAttack2);
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,15 @@ public class Player : MovingObject {
 	private Animator animator;
 	private int food;
 
+	public AudioClip moveSound1;
+	public AudioClip moveSound2;
+	public AudioClip eatSound1;
+	public AudioClip eatSound2;
+	public AudioClip drinkSound1;
+	public AudioClip drinkSound2;
+	public AudioClip gameOverSound;
+
+	private Vector2 touchOrigin = -Vector2.one;
 
 	// Use this for initialization
 	protected override void Start () {
@@ -43,7 +52,7 @@ public class Player : MovingObject {
 		RaycastHit2D hit;
 
 		if (Move (xDir, yDir, out hit)) {
-			//TODO sound
+			SoundManager.instance.RandomizeSfx (moveSound1, moveSound2);
 		}
 		CheckIfGameOver ();
 		GameManager.instance.playerTurn = false;
@@ -56,10 +65,12 @@ public class Player : MovingObject {
 		} else if (other.tag == "Food") {
 			food += pointsPerFood;
 			foodText.text = "+" + pointsPerFood + " Food :" + food;
+			SoundManager.instance.RandomizeSfx (eatSound1, eatSound2);
 			other.gameObject.SetActive (false);
 		} else if (other.tag == "Soda") {
 			food += pointsPerSoda;
 			foodText.text = "+" + pointsPerSoda + " Food :" + food;
+			SoundManager.instance.RandomizeSfx (drinkSound1, drinkSound2);
 			other.gameObject.SetActive (false);
 		}
 	}
@@ -77,6 +88,8 @@ public class Player : MovingObject {
 
 	private void CheckIfGameOver(){
 		if (food <= 0) {
+			SoundManager.instance.PlaySingle (gameOverSound);
+			SoundManager.instance.musicSource.Stop ();
 			GameManager.instance.GameOver ();
 		}
 	}
@@ -89,10 +102,36 @@ public class Player : MovingObject {
 		int horizontal = 0;
 		int vertical = 0;
 
+		#if UNITY_EDITOR
 		horizontal = (int)(Input.GetAxisRaw ("Horizontal"));
 		vertical = (int)(Input.GetAxisRaw ("Vertical"));
 		if (horizontal != 0)
 			vertical = 0;
+		#elif UNITY_ANDROID
+		if(Input.touchCount > 0){
+			Touch myTouch = Input.touches[0];
+			if(myTouch.phase == TouchPhase.Began){
+				touchOrigin = myTouch.position;
+			}
+			else if(myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0){
+				Vector2 touchEnd = myTouch.position;
+
+				float x = touchEnd.x - touchOrigin.x;
+				float y = touchEnd.y - touchOrigin.y;
+
+				touchOrigin.x = -1;
+
+				if (Mathf.Abs(x) > Mathf.Abs(y)){
+					horizontal = x>0 ? 1 : -1;
+				}
+				else{
+					vertical = y > 0? 1: -1;
+				}
+			}
+		}
+		#endif
+
+
 		if (horizontal != 0 || vertical != 0) {
 			AttemptMove<Wall> (horizontal, vertical);
 		}
